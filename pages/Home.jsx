a8df15a500bbca191bf8719e97a3f09ad676926a
sarthaksavarn816-gif <sarthaksavarn816@gmail.com>
2026-03-26 15:08:34 +0530

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,8 +4,10 @@ class Home extends React.Component {
   render() {
     return (
       <div className="page">
-        <h1>{this.props.title || 'Home'}</h1>
-        <p>This is my home page</p>
+        <h1>Full restaurant management system</h1>
+        <p> this project will include all the features needed for a complete restaurant management solution.
+          such as QR integrated menu, menu management, order processing, inventory tracking, staff management, and customer relationship management. The system will be designed to streamline restaurant operations, improve efficiency, and enhance the overall dining experience for customers. It will also include features for reporting and analytics to help restaurant owners make informed business decisions.
+        </p>
       </div>
     );
   }
