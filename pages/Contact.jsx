5e79ebebc9b78233b12e5c2f3501030b2e185d45
sarthaksavarn816-gif <sarthaksavarn816@gmail.com>
2026-03-26 15:08:09 +0530

    Update Contact.jsx

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -4,8 +4,8 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="page">
-        <h1>{this.props.title || 'Contact'}</h1>
-        <p>This is my contact page</p>
+        <h1>{this.props.title || 'contact us at'}</h1>
+        <p> sarthaksavarn@gmail.com</p>
       </div>
     );
   }
