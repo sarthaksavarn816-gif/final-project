38412a547b58e0e49b9d90c6b746e79105c6084c
sarthaksavarn816-gif <sarthaksavarn816@gmail.com>
2026-03-26 15:07:46 +0530

    Update About.jsx

--- pages/About.jsx
+++ pages/About.jsx
@@ -4,8 +4,8 @@ class About extends React.Component {
   render() {
     return (
       <div className="page">
-        <h1>{this.props.title || 'About'}</h1>
-        <p>This is my about page</p>
+        <h1>{this.props.title || 'About us'}</h1>
+        <p>we are building a project for a full restaurant management system</p>
       </div>
     );
   }
